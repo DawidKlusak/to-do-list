d20e292a6c2d5dd838761a167f7d6441e813db43
Dawid Klusak <dawidklusakbusiness@gmail.com>
2023-06-17 15:08:59 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+{
+    const tasks = [
+        {
+            content: "wypić piwo",
+            done: false,   
+        },
+        {
+            content: "zrobić sernik",
+            done: true,   
+        },
+    ];
+
+    const render = () => {
+        let htmlString = "";
+    }
+
+    const init= () => {
+
+    };
+
+    init();
+
+}
