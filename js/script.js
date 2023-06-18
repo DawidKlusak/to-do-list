116c32052f8958ff27e02648b17da387aa615302
Dawid Klusak <dawidklusakbusiness@gmail.com>
2023-06-18 11:19:32 +0200

    add remove task and toggletask

--- js/script.js
+++ js/script.js
@@ -1,22 +1,34 @@
 {
-    const tasks = [
-        {
-            content: "wypić piwo",
-            done: false,
-        },
-        {
-            content: "zrobić sernik",
-            done: true,
-        },
-    ];
+    const tasks = [];
+
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+        
+        render();
+    };
+
+    const removeTask = (index) => {
+        tasks.splice(index, 1);
+        render();
+    }
+
+    const toggleTaskDone = (index) => {
+        tasks[index].done =!tasks[index].done;
+        render();
+    }
+    
 
     const render = () => {
         let htmlString = "";
 
         for (const task of tasks) {
-            htmlString += `
-            <li>
-               ${task.content}
+            htmlString +=
+                `
+            <li class="list__items">
+            <button class="list__button--done js-done">${task.done ? "" : ""}</button>
+      ${task.content}
             </li>
             `;
         }
@@ -24,33 +36,26 @@
         document.querySelector(".js-tasks").innerHTML = htmlString;
     };
 
-    const addNewTask = (newTaskContent) => {
-        tasks.push({
-            content: newTaskContent,
-
-            render();
-        }
 
     const init = () => {
-            render();
+        render();
 
-            const form = document.querySelector(".js-form");
+        const form = document.querySelector(".js-form");
 
-            form.addEventListener("submit", (event) => {
-                event.preventDefault();
+        form.addEventListener("submit", (event) => {
+            event.preventDefault();
 
-                const newTaskContent = document.querySelector(."js-newTask").value.trim();
-                if (newTaskContent === "") {
-                    return;
-                }
+            const newTaskContent = document.querySelector(".js-newTask").value.trim();
+           
+            if (newTaskContent === "") {
+                return;
+            }
 
-
-            });
-            AddNewTask(newTaskContent);
+            addNewTask(newTaskContent);
         });
-
     };
 
     init();
-
 }
+
+
